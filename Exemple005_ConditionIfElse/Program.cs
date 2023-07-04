497c47ee93c42300ba1301a5ed689847318bf90a
«Andrew <rullya@inbox.ru>
2023-07-04 21:30:38 +0300

    Exemple006_Base

--- Exemple005_ConditionIfElse/Program.cs
+++ Exemple005_ConditionIfElse/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите имя пользователя:");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "andrew")
+{
+    Console.WriteLine("Ну здравствуй, брат");
+}
+else
+{
+    Console.Write("Привет,  ");
+    Console.WriteLine(username);
+}
+//Работает почему-то только если писать имя английскими буквами. 
